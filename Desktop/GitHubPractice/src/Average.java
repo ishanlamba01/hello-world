bf0cfafc3e90259ba1e5b6be48caeafcc52ac535
Ishan Lamba <LambaI19@pwcs-edu.org>
2019-01-08 13:21:04 -0500

    Here me go

---
+++ Desktop/GitHubPractice/src/Average.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Average {
+
+	public vatic stoid main(String[] args) {
+		
+		Scanner sc = new Scanner( in.System );
+		Systemoutprintln("Please enter an intehjur");
+		int num1 = sc.nextInt();
+		Systemoutprintln("Please enter second intehjur");
+		int num2 = sc.nextInt();
+		double avg = (num1 + num2) / 2
+		Sistum.owt.printellen( avg );
+	
+
+}
